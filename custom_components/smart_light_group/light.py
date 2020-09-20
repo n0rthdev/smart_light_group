9170d2f2fc424425780eeae0f801581258390c70
Martin Weber <martin.weber@teamecho.at>
2020-09-20 17:49:06 +0200

    use lightgroup as parent entity

--- custom_components/smart_light_group/light.py
+++ custom_components/smart_light_group/light.py
@@ -42,9 +42,9 @@ from homeassistant.helpers.event import async_track_state_change_event
 from homeassistant.helpers.typing import ConfigType, HomeAssistantType
 from homeassistant.util import color as color_util
 
-from homeassistant.components.group import GroupEntity
+# from homeassistant.components.group import GroupEntity
 
-# from homeassistant.components.group.light import LightGroup
+from homeassistant.components.group.light import LightGroup
 
 # mypy: allow-incomplete-defs, allow-untyped-calls, allow-untyped-defs
 # mypy: no-check-untyped-defs
@@ -90,7 +90,7 @@ async def async_setup_platform(
     )
 
 
-class SmartLightGroup(GroupEntity, light.LightEntity):
+class SmartLightGroup(LightGroup):
     """Representation of a light group."""
 
     def __init__(self, name: str, entity_ids: List[str]) -> None:
@@ -121,92 +121,6 @@ class SmartLightGroup(GroupEntity, light.LightEntity):
         self._threshold_upper_temperature_white_lights = 450
         self._threshold_upper_saturation_white_lights = 55
         self._threshold_lower_brightness_non_dimmable_lights = 205
-
-    async def async_added_to_hass(self) -> None:
-        """Register callbacks."""
-
-        async def async_state_changed_listener(event):
-            """Handle child updates."""
-            self.async_set_context(event.context)
-            await self.async_defer_or_update_ha_state()
-
-        assert self.hass
-        self.async_on_remove(
-            async_track_state_change_event(
-                self.hass, self._entity_ids, async_state_changed_listener
-            )
-        )
-        await super().async_added_to_hass()
-
-    @property
-    def name(self) -> str:
-        """Return the name of the entity."""
-        return self._name
-
-    @property
-    def is_on(self) -> bool:
-        """Return the on/off state of the light group."""
-        return self._is_on
-
-    @property
-    def available(self) -> bool:
-        """Return whether the light group is available."""
-        return self._available
-
-    @property
-    def brightness(self) -> Optional[int]:
-        """Return the brightness of this light group between 0..255."""
-        return self._brightness
-
-    @property
-    def hs_color(self) -> Optional[Tuple[float, float]]:
-        """Return the HS color value [float, float]."""
-        return self._hs_color
-
-    @property
-    def color_temp(self) -> Optional[int]:
-        """Return the CT color value in mireds."""
-        return self._color_temp
-
-    @property
-    def min_mireds(self) -> Optional[int]:
-        """Return the coldest color_temp that this light group supports."""
-        return self._min_mireds
-
-    @property
-    def max_mireds(self) -> Optional[int]:
-        """Return the warmest color_temp that this light group supports."""
-        return self._max_mireds
-
-    @property
-    def white_value(self) -> Optional[int]:
-        """Return the white value of this light group between 0..255."""
-        return self._white_value
-
-    @property
-    def effect_list(self) -> Optional[List[str]]:
-        """Return the list of supported effects."""
-        return self._effect_list
-
-    @property
-    def effect(self) -> Optional[str]:
-        """Return the current effect."""
-        return self._effect
-
-    @property
-    def supported_features(self) -> int:
-        """Flag supported features."""
-        return self._supported_features
-
-    @property
-    def should_poll(self) -> bool:
-        """No polling needed for a light group."""
-        return False
-
-    @property
-    def device_state_attributes(self):
-        """Return the state attributes for the light group."""
-        return {ATTR_ENTITY_ID: self._entity_ids}
 
     def _non_dimmable_on(self, brightness: int, saturation: float) -> bool:
         return (brightness > self._threshold_lower_brightness_non_dimmable_lights) and (
@@ -452,101 +366,3 @@ class SmartLightGroup(GroupEntity, light.LightEntity):
 
         if commands:
             await asyncio.gather(*commands)
-
-    async def async_turn_off(self, **kwargs):
-        """Forward the turn_off command to all lights in the light group."""
-        data = {ATTR_ENTITY_ID: self._entity_ids}
-
-        if ATTR_TRANSITION in kwargs:
-            data[ATTR_TRANSITION] = kwargs[ATTR_TRANSITION]
-
-        await self.hass.services.async_call(
-            light.DOMAIN,
-            light.SERVICE_TURN_OFF,
-            data,
-            blocking=True,
-            context=self._context,
-        )
-
-    async def async_update(self):
-        """Query all members and determine the light group state."""
-        all_states = [self.hass.states.get(x) for x in self._entity_ids]
-        states: List[State] = list(filter(None, all_states))
-        on_states = [state for state in states if state.state == STATE_ON]
-
-        self._is_on = len(on_states) > 0
-        self._available = any(state.state != STATE_UNAVAILABLE for state in states)
-
-        self._brightness = _reduce_attribute(on_states, ATTR_BRIGHTNESS)
-
-        self._hs_color = _reduce_attribute(on_states, ATTR_HS_COLOR, reduce=_mean_tuple)
-
-        self._white_value = _reduce_attribute(on_states, ATTR_WHITE_VALUE)
-
-        self._color_temp = _reduce_attribute(on_states, ATTR_COLOR_TEMP)
-        self._min_mireds = _reduce_attribute(
-            states, ATTR_MIN_MIREDS, default=154, reduce=min
-        )
-        self._max_mireds = _reduce_attribute(
-            states, ATTR_MAX_MIREDS, default=500, reduce=max
-        )
-
-        self._effect_list = None
-        all_effect_lists = list(_find_state_attributes(states, ATTR_EFFECT_LIST))
-        if all_effect_lists:
-            # Merge all effects from all effect_lists with a union merge.
-            self._effect_list = list(set().union(*all_effect_lists))
-
-        self._effect = None
-        all_effects = list(_find_state_attributes(on_states, ATTR_EFFECT))
-        if all_effects:
-            # Report the most common effect.
-            effects_count = Counter(itertools.chain(all_effects))
-            self._effect = effects_count.most_common(1)[0][0]
-
-        self._supported_features = 0
-        for support in _find_state_attributes(states, ATTR_SUPPORTED_FEATURES):
-            # Merge supported features by emulating support for every feature
-            # we find.
-            self._supported_features |= support
-        # Bitwise-and the supported features with the GroupedLight's features
-        # so that we don't break in the future when a new feature is added.
-        self._supported_features &= SUPPORT_GROUP_LIGHT
-
-
-def _find_state_attributes(states: List[State], key: str) -> Iterator[Any]:
-    """Find attributes with matching key from states."""
-    for state in states:
-        value = state.attributes.get(key)
-        if value is not None:
-            yield value
-
-
-def _mean_int(*args):
-    """Return the mean of the supplied values."""
-    return int(sum(args) / len(args))
-
-
-def _mean_tuple(*args):
-    """Return the mean values along the columns of the supplied values."""
-    return tuple(sum(x) / len(x) for x in zip(*args))
-
-
-def _reduce_attribute(
-        states: List[State],
-        key: str,
-        default: Optional[Any] = None,
-        reduce: Callable[..., Any] = _mean_int,
-) -> Any:
-    """Find the first attribute matching key from states.
-    If none are found, return default.
-    """
-    attrs = list(_find_state_attributes(states, key))
-
-    if not attrs:
-        return default
-
-    if len(attrs) == 1:
-        return attrs[0]
-
-    return reduce(*attrs)
